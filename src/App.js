9f84b7765fc7c5c9ec0959d939c4532fe7adbc36
Anmol-readwhere <anmol.rajas@mediologysoftware.com>
2024-09-09 17:03:10 +0530

    AddUser Component

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import Plugins from './Components/Plugins';
 import User from './Components/User';
 import Setting from './Components/Setting';
 
+import AddUser from './Components/AddUser';
+
 function App() {
   return (
     <>
      <BrowserRouter>
     <Sidebar />
+    <AddUser/>
      <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/create" element={<Create />} />
           <Route path="/media" element={<Media />} />
           <Route path="/analytics" element={<Analytics />} />
-              <Route path="/comments" element={<Comment />} />
-              <Route path="/plugins" element={<Plugins />} />
-              <Route path="/user" element={<User />} />
-              <Route path="/setting" element={<Setting />} />
-            {/* <Route path="*" element={<Errorpage />} /> */}
+          <Route path="/comments" element={<Comment />} />
+          <Route path="/plugins" element={<Plugins />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/user/adduser" element={<AddUser />}/>
+        {/* <Route path="*" element={<Errorpage />} /> */}
         </Routes>
      </BrowserRouter>
     
